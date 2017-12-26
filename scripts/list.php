520089b2723549b16de36f5003d3ebdaf56db201
JoshyPHP <s9e.dev@gmail.com>
2017-12-26 15:04:50 +0100

    Added script

---
+++ scripts/list.php
@@ -0,0 +1,15 @@
+#!/usr/bin/php
+<?php
+
+$dom = new DOMDocument;
+$dom->load(__DIR__ . '/../addon/_data/bb_code_media_sites.xml');
+$sites = $dom->getElementsByTagName('site');
+
+$text = 'This add-on contains the definitions for [b]' . $sites->length . ' media sites[/b]: ';
+foreach ($sites as $site)
+{
+	$text .= $site->getAttribute('site_title') . ', ';
+}
+$text = substr($text, 0, -2) . '.';
+
+echo "$text\n";
