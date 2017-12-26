dc0e56e79796a2cb9218aea035a200cbc1452e42
JoshyPHP <s9e.dev@gmail.com>
2017-12-26 15:00:22 +0100

    Updated script

---
+++ scripts/release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd "$(dirname $0)/.."
+php scripts/build.php
+rm -rf addon/_output
+echo "y" | php cmd.php xf-addon:upgrade s9e/MediaSites
+php cmd.php xf-addon:build-release s9e/MediaSites
+advzip -z4 "$(ls -1t addon/_releases/*.zip | head -n1)"
