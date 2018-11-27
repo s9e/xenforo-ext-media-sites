c6d618ed310d1ca286bfb5ad2ebdc8fedc1e2e63
JoshyPHP <s9e.dev@gmail.com>
2018-11-27 20:14:04 +0100

    Added add-on options

--- addon/Setup.php
+++ addon/Setup.php
@@ -9,14 +9,11 @@ namespace s9e\MediaSites;
 
 use XF;
 use XF\AddOn\AbstractSetup;
+use XF\Entity\Option;
 
 class Setup extends AbstractSetup
 {
 	public function install(array $stepParams = [])
-	{
-	}
-
-	public function upgrade(array $stepParams = [])
 	{
 	}
 
@@ -31,5 +28,36 @@ class Setup extends AbstractSetup
 			],
 			false
 		);
+	}
+
+	public function upgrade(array $stepParams = [])
+	{
+	}
+
+	public static function validateFooter($newValue, Option $option)
+	{
+		self::setTemplateModification($option, 'Footer', ($newValue === 'show'));
+
+		return true;
+	}
+
+	public static function validateYouTubePrivacy($newValue, Option $option)
+	{
+		self::setTemplateModification($option, 'YouTube_Privacy', (bool) $newValue);
+
+		return true;
+	}
+
+	protected static function setTemplateModification(Option $option, $key, $enabled)
+	{
+		$entity = $option->em()
+			->getFinder('XF:TemplateModification')
+			->where('modification_key', 's9e_MediaSites_' . $key)
+			->fetchOne();
+		if ($entity)
+		{
+			$entity->set('enabled', $enabled);
+			$entity->save();
+		}
 	}
 }
