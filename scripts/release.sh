144a2f8de4542e25e5943a7bbcd69ddae66231de
JoshyPHP <s9e.dev@gmail.com>
2018-12-07 17:33:01 +0100

    Updated release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd "$(dirname $0)/.."
-#php cmd.php xf-addon:bump-version s9e/MediaSites
 php scripts/build.php
-rm -rf addon/_output
+rm -rf addon/_output addon/hashes.json
 echo "y" | php cmd.php xf-addon:upgrade s9e/MediaSites
 php cmd.php xf-addon:build-release s9e/MediaSites
 advzip -z4 "$(ls -1t addon/_releases/*.zip | head -n1)"
