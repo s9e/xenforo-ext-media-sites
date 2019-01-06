b5ad8ee5f6e8e5ae0f0e0cab7ae6af74a04e234e
JoshyPHP <s9e.dev@gmail.com>
2019-01-06 19:00:28 +0100

    Updated release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,4 +4,10 @@ php scripts/build.php
 rm -rf addon/_output addon/hashes.json
 echo "y" | php cmd.php xf-addon:upgrade s9e/MediaSites
 php cmd.php xf-addon:build-release s9e/MediaSites
-advzip -z4 "$(ls -1t addon/_releases/*.zip | head -n1)"
+
+cd addon/_releases
+file="$(ls -1t *.zip | head -n1)"
+rm -rf upload
+unzip "$file"
+kzip -r -y "$file" upload
+advzip -z4 "$file"
