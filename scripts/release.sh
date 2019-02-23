8741df184fbd75a03db452483d13bc2f2bb6468f
JoshyPHP <s9e.dev@gmail.com>
2019-02-23 02:09:47 +0100

    Updated release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,7 +8,6 @@ php cmd.php xf-addon:build-release s9e/MediaSites
 cd addon/_releases
 file="$(ls -1t *.zip | head -n1)"
 rm -rf upload
-unzip -q "$file"          && \
-kzip -r -y "$file" upload && \
-advzip -z4 "$file"        && \
+unzip -q "$file"                && \
+advzip -a4 -i100 "$file" upload && \
 rm -rf upload
