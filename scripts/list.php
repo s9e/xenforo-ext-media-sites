eb33a371977d47d60cd20b8e3e44be62797766aa
JoshyPHP <s9e.dev@gmail.com>
2020-02-09 15:30:53 +0100

    Updated script

--- scripts/list.php
+++ scripts/list.php
@@ -5,12 +5,14 @@ $dom = new DOMDocument;
 $dom->load(__DIR__ . '/../addon/_data/bb_code_media_sites.xml');
 $sites = $dom->getElementsByTagName('site');
 
-$text = 'This add-on contains the definitions for [b]' . $sites->length . ' media sites[/b]: ';
+$names = [];
 foreach ($sites as $site)
 {
-	$text .= $site->getAttribute('site_title') . ', ';
+	$names[] = $site->getAttribute('site_title');
 }
+sort($names, SORT_FLAG_CASE | SORT_NATURAL);
+
+$text = 'This add-on contains the definitions for [b]' . $sites->length . ' media sites[/b]: ' . implode(', ', $names) . '.';
 $text = str_replace('BitChute', 'Bit Chute', $text);
-$text = substr($text, 0, -2) . '.';
 
 echo "$text\n";
