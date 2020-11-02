9f6bd1335ac27a97987f866bce1a8f06c5fb1fb0
JoshyPHP <s9e.dev@gmail.com>
2020-11-02 16:50:05 +0100

    WiP

--- src/AddonBuilder.php
+++ src/AddonBuilder.php
@@ -92,7 +92,7 @@ class AddonBuilder
 		$this->sites         = iterator_to_array($this->configurator->MediaEmbed->defaultSites);
 		$this->xfTranspiler  = new XenForoTemplate;
 
-		$this->configurator->templateNormalizer->add(new SwitchCSSWidth);
+		$this->configurator->templateNormalizer->prepend(new SwitchCSSWidth);
 		$this->configurator->templateNormalizer->add(new RemoveDefaultStyle);
 		$this->configurator->templateNormalizer->add(new RemoveLazyLoading);
 		$this->configurator->templateNormalizer->add(new SetGitHubIframeApiVersion);
