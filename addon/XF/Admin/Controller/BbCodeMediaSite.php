31da0d0ff21314c99ac0d492e9cf7cadd1ecf91d
JoshyPHP <s9e.dev@gmail.com>
2021-01-04 15:06:27 +0100

    Bumped copyright year

--- addon/XF/Admin/Controller/BbCodeMediaSite.php
+++ addon/XF/Admin/Controller/BbCodeMediaSite.php
@@ -2,7 +2,7 @@
 
 /**
 * @package   s9e\MediaSites
-* @copyright Copyright (c) 2017-2020 The s9e authors
+* @copyright Copyright (c) 2017-2021 The s9e authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\MediaSites\XF\Admin\Controller;
