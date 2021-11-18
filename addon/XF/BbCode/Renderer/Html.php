71d3e19cfbe7940ca194328a2499f31da9df4a76
JoshyPHP <s9e.dev@gmail.com>
2021-11-18 19:25:18 +0100

    Do not trim newlines after a MEDIA BBCode from this add-on

--- addon/XF/BbCode/Renderer/Html.php
+++ addon/XF/BbCode/Renderer/Html.php
@@ -9,6 +9,17 @@ namespace s9e\MediaSites\XF\BbCode\Renderer;
 
 class Html extends XFCP_Html
 {
+	public function renderTag(array $tag, array $options)
+	{
+		$html = parent::renderTag($tag, $options);
+		if ($this->trimAfter === 1 && strpos($html, 'data-s9e-mediaembed') !== false)
+		{
+			$this->trimAfter = 0;
+		}
+
+		return $html;
+	}
+
 	public function renderTagUrl(array $children, $option, array $tag, array $options)
 	{
 		if (is_array($option) && isset($option['media']) && preg_match('(^(\\w+):(.+))', $option['media'], $m))
