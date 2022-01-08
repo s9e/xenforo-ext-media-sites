878a2c1ac7dd9930dec29094afbc49d8ef8acc93
JoshyPHP <s9e.dev@gmail.com>
2022-01-08 22:59:42 +0100

    Added Composer commands

--- scripts/build.php
+++ scripts/build.php
@@ -1,7 +1,22 @@
 #!/usr/bin/php
-<?php
+<?php declare(strict_types=1);
+
+use s9e\AddonBuilder\MediaSites\AddonBuilder;
+use s9e\TextFormatter\Configurator;
+use s9e\TextFormatter\Plugins\MediaEmbed\Configurator\Collections\XmlFileDefinitionCollection;
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
-$builder = new s9e\AddonBuilder\MediaSites\AddonBuilder;
+$addonId  = $_SERVER['ADDON_ID']  ?? 's9e/MediaSites';
+$addonDir = $_SERVER['ADDON_DIR'] ?? __DIR__ . '/../addon';
+$sitesDir = $_SERVER['SITES_DIR'] ?? null;
+
+$configurator = new Configurator;
+if (isset($sitesDir))
+{
+	$configurator->MediaEmbed->defaultSites = new XmlFileDefinitionCollection($sitesDir);
+}
+
+$builder = new AddonBuilder($addonDir, $configurator);
+$builder->nsRoot = strtr($addonId, '/', '\\');
 $builder->build();
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+addonId="${1-s9e/MediaSites}"
+
 cd "$(dirname $0)/.."
 php scripts/build.php                                  || exit
 rm -rf addon/_output addon/hashes.json                 || exit
-echo "y" | php cmd.php xf-addon:upgrade s9e/MediaSites
-php cmd.php xf-addon:build-release s9e/MediaSites
+echo "y" | php cmd.php xf-addon:upgrade "$addonId"
+php cmd.php xf-addon:build-release "$addonId"
 
 cd addon/_releases
 file="$(ls -1t *.zip | head -n1)"
