03462b2eeffb86fae78a81daf68b45f65d37c0eb
JoshyPHP <s9e.dev@gmail.com>
2022-03-03 11:41:31 +0100

    Count overloaded URL tags as MEDIA tag
    Fixes #3

--- addon/XF/BbCode/ProcessorAction/AnalyzeUsage.php
+++ addon/XF/BbCode/ProcessorAction/AnalyzeUsage.php
@@ -9,9 +9,18 @@ namespace s9e\MediaSites\XF\BbCode\ProcessorAction;
 
 use XF;
 use XF\BbCode\Processor;
+use XF\BbCode\ProcessorAction\AnalyzerHooks;
 
 class AnalyzeUsage extends XFCP_AnalyzeUsage
 {
+	public function addAnalysisHooks(AnalyzerHooks $hooks)
+	{
+		parent::addAnalysisHooks($hooks);
+
+		// Overloaded [URL media=""] tags are always rendered so they should always be analyzed
+		$hooks->addTagHook('url', 'analyzeUrlMediaTag');
+	}
+
 	public function analyzeUnfurlUsage($string, Processor $processor)
 	{
 		// Remove the media attribute from [URL unfurl="true" tags to make them look like regular
@@ -20,5 +29,13 @@ class AnalyzeUsage extends XFCP_AnalyzeUsage
 		$string = preg_replace($regexp, '', $string);
 
 		parent::analyzeUnfurlUsage($string, $processor);
+	}
+
+	public function analyzeUrlMediaTag(array $tag)
+	{
+		if (!empty($tag['option']['media']))
+		{
+			$this->adjustTagCount('media', 1);
+		}
 	}
 }
