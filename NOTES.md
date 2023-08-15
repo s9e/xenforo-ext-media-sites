2b6b834d1e9ea5df691287eeffcf1c282fa60388
JoshyPHP <s9e.dev@gmail.com>
2023-08-15 23:07:27 +0200

    Added suffix option to custom [URL] BBCode

---
+++ NOTES.md
@@ -0,0 +1,32 @@
+This document contains notes related to development, for future reference and in no particular order.
+
+
+### Adding a link when using a custom `[URL]` BBCode to display media
+
+Could be enabled:
+
+ 1. Unconditionally.
+ 2. If `autoEmbedMedia` is set to add a link.
+ 3. Via its own option.
+
+(1) and (2) ends up with people asking for an option down the line.
+
+
+The markup for the link can be provided:
+
+ 1. As HTML, via a template.
+    1. Hard to edit.
+    2. The link won't follow rules associated with `[URL]` tags, `rel` won't be correct, extensions that track outgoing links may not work.
+ 2. As HTML, as an option.
+ 3. As BBCode.
+    1. Using `autoEmbedMedia.linkBbCode` means this feature depends on `autoEmbedMedia`.
+	   1. Disabling auto-embed *resets* `autoEmbedMedia.linkBbCode` to its default value. That's own togglable textboxes work in XenForo.
+ 4. No markup at all, just render both the original `[URL]` BBCode and the embedded media. Use CSS for styling.
+    1. Hard to configure.
+
+
+BBCode pitfalls:
+
+ 1. The BBCode suffix could be infinitely recursive if `[URL media=...]` is used.
+ 2. BBCode injection via the URL. Square brackets and quotes can be escaped to mitigate.
+ 3. Long URLs are not shortened. That's how `autoEmbedMedia` behaves as well.
--- addon/XF/BbCode/Renderer/Html.php
+++ addon/XF/BbCode/Renderer/Html.php
@@ -7,6 +7,8 @@
 */
 namespace s9e\MediaSites\XF\BbCode\Renderer;
 
+use XF;
+
 class Html extends XFCP_Html
 {
 	public function renderTag(array $tag, array $options)
@@ -27,10 +29,60 @@ class Html extends XFCP_Html
 			$html = $this->renderTagMedia([$m[2]], $m[1], $tag, []);
 			if ($html !== '')
 			{
+				$html .= $this->renderMediaSuffix($children, $options);
+
 				return $html;
 			}
 		}
 
 		return parent::renderTagUrl($children, $option, $tag, $options);
+	}
+
+	protected function removeMarkupFromSubTree(array $tree): array
+	{
+		$cleanTree = [];
+		foreach ($tree as $element)
+		{
+			if (is_array($element))
+			{
+				// Replace the original markup with empty strings
+				$element['original'] = ['', ''];
+				$element['children'] = $this->removeMarkupFromSubTree($element['children']);
+			}
+
+			$cleanTree[] = $element;
+		}
+
+		return $cleanTree;
+	}
+
+	protected function renderMediaSuffix(array $children, array $options): string
+	{
+		$suffixOptions  = XF::options()->s9e_MediaSites_Url_Suffix ?? [];
+		$suffixOptions += [
+			'bbcode'  => '[i][size=2][url={$url}]View: {$url}[/url][/size][/i]',
+			'enabled' => true
+		];
+		if (!$suffixOptions['enabled'])
+		{
+			return '';
+		}
+
+		// NOTE: $children will usually contain 1 string but may contain any number of array|string
+		//       if the [URL] tag contains markup, e.g. [URL][B]...[/B][URL]
+		$url = $this->renderSubTreePlain($this->removeMarkupFromSubTree($children), $options);
+
+		// Escape all characters that could interfere with BBCodes as a precaution
+		$url = strtr($url, [' ' => '%20', '"' => '%22', "'" => '%27', '[' => '%5B', ']' => '%5D']);
+
+		$bbcodeSuffix = "\n" . str_replace('{$url}', $url, $suffixOptions['bbcode']);
+
+		/**
+		* @see XF\BbCode\Traverser::renderSubTree()
+		*/
+		return $this->renderSubTree(
+			XF::app()->bbCode()->parser()->parse($bbcodeSuffix, $this->rules),
+			$options
+		);
 	}
 }
