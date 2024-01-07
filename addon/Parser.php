c805cc1a904912a1775efd140a3a93e22f7b808d
JoshyPHP <s9e.dev@gmail.com>
2024-01-07 13:30:39 +0100

    Updated dependencies

--- addon/Parser.php
+++ addon/Parser.php
@@ -151,7 +151,7 @@ class Parser
 		'theatlantic'=>[['!theatlantic\\.com/video/index/(?<id>\\d+)!']],
 		'theguardian'=>[['!theguardian\\.com/(?<id>\\w+/video/20(?:0[0-9]|1[0-7])[-/\\w]+)!']],
 		'theonion'=>[['!theonion\\.com/video/[-\\w]+[-,](?<id>\\d+)!']],
-		'threads'=>[['!threads\\.net/(?:@[-\\w]+/pos)?t/(?<id>[-\\w]+)!']],
+		'threads'=>[['!threads\\.net/(?:@[-\\w.]+/pos)?t/(?<id>[-\\w]+)!']],
 		'tiktok'=>[['#tiktok\\.com/(?:@[.\\w]+/video|v|(?:i18n/)?share/video)/(?<id>\\d+)#'],[],[['extract'=>['#tiktok\\.com/(?:@[.\\w]+/video|v|(?:i18n/)?/share/video)/(?<id>\\d+)#'],'match'=>["#//v[mt]\\.tiktok\\.com/(?'short_id'\\w+)#","#tiktok\\.com/t/(?'short_id'\\w+)#"],'url'=>'https://www.tiktok.com/t/{@short_id}']]],
 		'tmz'=>[['@tmz\\.com/videos/(?<id>\\w+)@']],
 		'tradingview'=>[['!tradingview\\.com/(?:chart/[^/]+|i)/(?<chart>\\w+)!','!tradingview\\.com/symbols/(?<symbol>[-:\\w]+)!']],
